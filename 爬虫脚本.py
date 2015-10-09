08f1d79b14984cd1ea6cc128c55ee2f8b1ee61a5
Akring <ddflj3210@gmail.com>
2015-10-09 16:16:11 +0800

    修改指向UI4APP

--- 爬虫脚本.py
+++ 爬虫脚本.py
@@ -14,7 +14,7 @@ def getImage(url, filePath):
     sys.setdefaultencoding('utf-8')
     html = urllib2.urlopen(url).read()
     soup = BeautifulSoup(html, "html.parser")
-    result = soup.findAll(attrs={'class': 'post-body'})
+    result = soup.findAll(attrs={'class': 'uiimg'})
     # print(result)
 
     for li in result:
@@ -34,15 +34,18 @@ def getImage(url, filePath):
                 # pos = filesavepath.find(".png")
                 # filesavepath = filesavepath[:pos] + "附件" + filesavepath[pos:]
                 print ("File already exists")
-                break
-            print link
-            urllib.urlretrieve(link, filesavepath)
+            else:
+                print link
+                urllib.urlretrieve(link, filesavepath)
 
 if __name__ == '__main__':
 
     print("start")
 
-    url = 'http://akring.github.io/2014/05/29/mac%E4%B8%8B%E7%9A%84%E5%88%87%E5%9B%BE%E7%A5%9E%E5%99%A8%E2%80%94%E2%80%94slicy%E4%BD%BF%E7%94%A8%E8%AF%B4%E6%98%8E/'
+    # url = 'http://akring.github.io/2014/05/29/mac%E4%B8%8B%E7%9A%84%E5%88%87%E5%9B%BE%E7%A5%9E%E5%99%A8%E2%80%94%E2%80%94slicy%E4%BD%BF%E7%94%A8%E8%AF%B4%E6%98%8E/'
+
+    url = 'http://ui4app.com/'
+
     filePath = '/Users/akring/Desktop/pic'
 
     getImage(url, filePath)
